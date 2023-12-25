fc1b646554370e1dd5ddf69cf256837cd25e1559
Kerem Görkem Görgülü <gorkemgorgulu003@gmail.com>
2023-12-25 14:31:07 +0300

    Functional CRUD operations for "PetOwner" entity has been implemented.

--- src/main/java/com/production/ehayvanbackendapi/DTO/request/CreateOrUpdatePetOwnerDTO.java
+++ src/main/java/com/production/ehayvanbackendapi/DTO/request/CreateOrUpdatePetOwnerDTO.java
@@ -2,15 +2,6 @@ package com.production.ehayvanbackendapi.DTO.request;
 
 public class CreateOrUpdatePetOwnerDTO {
     private CreateOrUpdateCustomerDTO user;
-    private Integer veterinarianID;
-
-    public Integer getVeterinarianID() {
-        return veterinarianID;
-    }
-
-    public void setVeterinarianID(Integer veterinarianID) {
-        this.veterinarianID = veterinarianID;
-    }
 
     public CreateOrUpdateCustomerDTO getUser() {
         return user;
